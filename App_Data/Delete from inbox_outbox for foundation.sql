731121502ede8fb527c0eab2b7cda98d1d829574
yyoussef <yyoussef@mcit.gov.eg>
2015-02-19 11:49:47 +0200

    add delete foundation command

--- App_Data/Delete from inbox_outbox for foundation.sql
+++ App_Data/Delete from inbox_outbox for foundation.sql
@@ -74,3 +74,8 @@ delete     from Commission where foundation_id = @foundation_id
 
 --delete from Departments
 --where foundation_id = @foundation_id
+
+----------------------------------
+--delete from Foundations
+--where Foundation_ID = @foundation_id
+---------------------------------
